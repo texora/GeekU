014e2cecd26a4f1530626c002e1da42cde13557c
Kevin Bridges <kevin@wiiBridges.com>
2016-03-16 21:07:54 -0500

    implemented academicSubj.js

--- src/shared/model/academicSubj.js
+++ src/shared/model/academicSubj.js
@@ -1,31 +1,84 @@
 'use strict';
 
 /**
- * Promotes both AcademicGroups Subjects and the relationship between
+ * Promotes both AcademicGroups, Subjects, and the relationship between
  * them (AcademicGroups --1:M-- Subjects).
  *
- * This utility is used for validation, and GUI drivers for lists, etc.
+ * This utility is used for validation (client/server), and client-side 
+ * GUI drivers for lists, etc.
  */
 
-// for each academicGroup, an array of subjects
-const academicGroups = {
-  "Engineering": ["AEP", "BME", "CHEME", "CEE", "CS", "EAS", "ECE", "ENGRC", "ENGRD", "ENGRG", "ENGRI", "INFO", "MSE", "MAE", "NSE", "ORIE", "STSCI", "SYSEN"],
-  "Graduate Management": ["NBA", "NCC", "NMI", "NRE"],
-  "Arts and Sciences": [ "AMST", "ANTHR", "ARKEO", "ARTH", "ASTRO", "BSOC", "CHEM", "CLASS", "COGST", "ECON", "ENGL", "FREN", "GERST", "GOVT", "HIST", "JAPAN", "JPLIT", "JWST", "LATIN", "LING", "MATH", "MEDVL", "MUSIC", "PHIL", "PHYS", "PSYCH", "RELST", "STS", "WRIT",],
+
+//***
+//*** public API
+//***
+
+// promote all academicGroups <string[]>
+export function academicGroups() {
+  return _allAcademicGroups;
+}
+
+// does supplied academicGroup exist? <boolean>
+export function academicGroupExists(academicGroup) {
+  return _academicGroups[academicGroup] ? true : false;
+}
+
+// promote academicGroup for given subject <string>
+export function academicGroupForSubject(subject) {
+  const academicGroup = _subjects[subject];
+  if (!academicGroup)
+    throw new Error(`ERROR: academicGroupForSubject('${subject}') ... non-existent subject`);
+  return academicGroup;
+}
+
+// promote all subjects <string[]>
+export function subjects() {
+  return _allSubjects;
+}
+
+// does supplied subject exist? <boolean>
+export function subjectExists(subject) {
+  return _subjects[subject] ? true : false;
+}
+
+// promote subjects for given academicGroup <string[]>
+export function subjectsForAcademicGroup(academicGroup) {
+  const subjects = _academicGroups[academicGroup];
+  if (!subjects)
+    throw new Error(`ERROR: subjectsForAcademicGroup('${academicGroup}') ... non-existent academicGroup`);
+  return subjects;
+}
+
+
+
+//***
+//*** internals
+//***
+
+// for each academicGroup <string>, an array of subjects <string[]>
+const _academicGroups = {
+  'Engineering': ['AEP', 'BME', 'CHEME', 'CEE', 'CS', 'EAS', 'ECE', 'ENGRC', 'ENGRD', 'ENGRG', 'ENGRI', 'INFO', 'MSE', 'MAE', 'NSE', 'ORIE', 'STSCI', 'SYSEN'],
+  'Graduate Management': ['NBA', 'NCC', 'NMI', 'NRE'],
+  'Arts and Sciences': [ 'AMST', 'ANTHR', 'ARKEO', 'ARTH', 'ASTRO', 'BSOC', 'CHEM', 'CLASS', 'COGST', 'ECON', 'ENGL', 'FREN', 'GERST', 'GOVT', 'HIST', 'JAPAN', 'JPLIT', 'JWST', 'LATIN', 'LING', 'MATH', 'MEDVL', 'MUSIC', 'PHIL', 'PHYS', 'PSYCH', 'RELST', 'STS', 'WRIT',],
 };
 
-// for each subject, it's cooresponding academicGroups
-const subjects = {}; // ... machine genereated (below)
-for (let academicGroup in academicGroups) {
-  const subjs = academicGroups[academicGroup];
+// for each subject <string>, it's cooresponding academicGroups <string>
+const _subjects = {}; // ... machine genereated (below)
+
+// ALL subjects <string[]>
+const _allSubjects = []; // ... machine genereated (below)
+
+// ALL academicGroups <string[]>
+const _allAcademicGroups = []; // ... machine genereated (below)
+
+// machine generate various renditions, from the master definition in academicGroups
+for (let academicGroup in _academicGroups) {
+  const subjs = _academicGroups[academicGroup];
+  _allAcademicGroups.push(academicGroup);  // generate: _allAcademicGroups
   for (let subj of subjs) {
-    subjects[subj] = academicGroup;
+    _subjects[subj] = academicGroup;       // generate: _subjects
+    _allSubjects.push(subj);               // generate: _allSubjects
   }
 }
-
-// ? console.log("??? Wow: academicGroups: ", academicGroups);
-// ? console.log("??? Zee: subjects: ", subjects);
-
-export function academicGroupExists(academicGroup) {
-  return academicGroups[academicGroup] ? true : false;
-}
+_allAcademicGroups.sort();
+_allSubjects.sort();
--- src/shared/model/spec/academicSubj.spec.js
+++ src/shared/model/spec/academicSubj.spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
-import                            '../../util/polyfill.js';
-import expect                from 'expect';
-import {academicGroupExists} from '../academicSubj';
+import '../../util/polyfill.js';
+import expect         from 'expect';
+import * as underTest from '../academicSubj';
 
 
 // ***
@@ -11,12 +11,92 @@ import {academicGroupExists} from '../academicSubj';
 
 describe('academicSubj tests', () => {
 
-  it('academicGroupExists("NON Existant Academic Group")', () => {
-    expect(academicGroupExists("NON Existant Academic Group")).toEqual(false);
+
+  describe('academicGroups() tests', () => {
+    it('Array Check', () => {
+      expect(underTest.academicGroups())
+        .toBeA('array');
+    });
+
+    it('Expected academicGroup: Engineering', () => {
+      expect(underTest.academicGroups())
+        .toInclude('Engineering');
+    });
   });
 
-  it('academicGroupExists("Graduate Management")', () => {
-    expect(academicGroupExists("Graduate Management")).toEqual(true);
+
+  describe('academicGroupExists() tests', () => {
+    it('NON-EXIST-GROUP', () => {
+      expect(underTest.academicGroupExists('NON-EXIST-GROUP'))
+        .toEqual(false);
+    });
+
+    it('Expected Group: Engineering', () => {
+      expect(underTest.academicGroupExists('Engineering'))
+        .toEqual(true);
+    });
+  });
+
+
+  describe('academicGroupForSubject() tests', () => {
+    it('Bad Subject', () => {
+      expect( () => { underTest.academicGroupForSubject('NON-EXIST-SUBJ') })
+                               .toThrow(/non-existent subject/);
+    });
+
+    it('Expected Group', () => {
+      expect(underTest.academicGroupForSubject('CS'))
+                      .toEqual('Engineering');
+    });
+
+    it('Non Expected Group', () => {
+      expect(underTest.academicGroupForSubject('MUSIC'))
+                      .toNotEqual('Engineering');
+    });
+  });
+
+
+  describe('subjects() tests', () => {
+    it('Array Check', () => {
+      expect(underTest.subjects())
+        .toBeA('array');
+    });
+
+    it('Expected subject: CS', () => {
+      expect(underTest.subjects())
+        .toInclude('CS');
+    });
+  });
+
+
+  describe('subjectExists() tests', () => {
+    it('NON-EXIST-SUBJ', () => {
+      expect(underTest.subjectExists('NON-EXIST-SUBJ'))
+        .toEqual(false);
+    });
+
+    it('Expected Subject: CS', () => {
+      expect(underTest.subjectExists('CS'))
+        .toEqual(true);
+    });
+  });
+
+
+  describe('subjectsForAcademicGroup() tests', () => {
+    it('Bad Group', () => {
+      expect( () => { underTest.subjectsForAcademicGroup('NON-EXIST-GROUP') })
+        .toThrow(/non-existent academicGroup/);
+    });
+
+    it('Expected Subject', () => {
+      expect(underTest.subjectsForAcademicGroup('Engineering'))
+        .toInclude('CS');
+    });
+
+    it('Non Expected Subject', () => {
+      expect(underTest.subjectsForAcademicGroup('Engineering'))
+        .toExclude('MUSIC');
+    });
   });
 
 });
