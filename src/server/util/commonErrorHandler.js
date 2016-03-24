78f0d5f3e68bccf09e0c1ca55330ade5477fea24
Kevin Bridges <kevin@wiiBridges.com>
2016-03-24 09:34:49 -0500

    maintain a unique LogId to correlate client message with server log

--- src/server/util/commonErrorHandler.js
+++ src/server/util/commonErrorHandler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import shortid from 'shortid'
+
 //***
 //*** Our express common error handler (handles BOTH "throw Error" and Express "next(err)")
 //***
@@ -11,10 +13,12 @@ export default function commonErrorHandler(err, req, res, next) {
   const status    = err.status    || 500; // default to 500: Internal Error
   const message   = err.message   || "UNKNOWN MSG";
   const clientMsg = err.clientMsg || "Unexpected Condition"; // client msg is sanitized to not reveal internal details
+  const logId     = shortid.generate();
 
   // log problem in our server console
   console.error(`
 Encountered Error (GeekU Common Error Handler) ...
+  LogId:      ${logId}
   Name:       ${name}
   Status:     ${status}
   Client Msg: ${clientMsg}
@@ -26,11 +30,16 @@ Encountered Error (GeekU Common Error Handler) ...
 
   // handle programatic XMLHttpRequest related error responses
   if (req.xhr) {
-    res.status(status).send({ error: clientMsg} );
+    res.status(status).send({ 
+      error:   clientMsg,
+      status:  status,
+      name:    name,
+      logId:   logId
+    });
   }
   // handle all other 
   else {
-    res.status(status).send(clientMsg);
+    res.status(status).send(`${clientMsg} (LogId: ${logId})`);
   }
 
 }
