cd88019ce743fb397649c1d9a97c3505c7560f89
Kevin Bridges <kevin@wiiBridges.com>
2016-05-18 06:03:17 -0500

    applied initial splash screen

---
+++ src/client/ClientSidePolyfill.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import '../shared/util/polyfill'; // our standarad polyfill (for both client/server)
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Material-UI components use react-tap-event-plugin (onTouchTap event) 
+// to listen for touch events because onClick is not fast enough.
+// This dependency is temporary and can go away with react 1.0 release
+// ... https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
---
+++ src/client/Log4GeekU.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import Log                          from '../shared/util/Log';
+import registerInteractiveLogConfig from '../shared/util/LogInteractiveConfigForBrowser';
+
+/*--------------------------------------------------------------------------------
+   Configure logs for GeekUApp.
+
+   This module should be included very early (first) in the GeekUApp start-up 
+   initialization process.
+   --------------------------------------------------------------------------------*/
+
+const log = new Log('GeekU');
+
+// configure our initial Log Filter Settings
+const logConfig = Log.config({
+  filter: {
+    'root':    'INFO',
+    'GeekU':   'DEBUG', // 'DEBUG' to see entire Material-UI muiTheme
+  },
+
+  excludeClientErrors: false,  // false: to see client Errors in log
+  
+});
+
+// emit our current Log Configuration
+log.info(()=>`Current Log Configuration:\n${JSON.stringify(logConfig, null, 2)}`);
+
+// allow browser logs to be interactivally configured through Easter Egg key sequence of 'LogConfig'
+registerInteractiveLogConfig('LogConfig');
