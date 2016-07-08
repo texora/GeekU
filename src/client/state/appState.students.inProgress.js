953ac330f8cb13de8840300d118c0f9062e10d96
Kevin Bridges <kevin@wiiBridges.com>
2016-07-08 15:44:48 -0500

    prevent inProgress state from going negative (even if we have something out of sync)

--- src/client/state/appState.students.inProgress.js
+++ src/client/state/appState.students.inProgress.js
@@ -9,10 +9,17 @@ import ReduxSubReducer from '../util/ReduxSubReducer';
 // ***
 
 const subReducer = new ReduxSubReducer('appState.students.inProgress', {
-  [AT.retrieveStudents.start]    (inProgress, action) { return [inProgress+1, ()=>`setting inProgress to: ${inProgress+1}`]; },
-  [AT.retrieveStudents.complete] (inProgress, action) { return [inProgress-1, ()=>`setting inProgress to: ${inProgress-1}`]; },
-  [AT.retrieveStudents.fail]     (inProgress, action) { return [inProgress-1, ()=>`setting inProgress to: ${inProgress-1}`]; },
+  [AT.retrieveStudents.start]    (inProgress, action) { return addIn(inProgress, +1); },
+  [AT.retrieveStudents.complete] (inProgress, action) { return addIn(inProgress, -1); },
+  [AT.retrieveStudents.fail]     (inProgress, action) { return addIn(inProgress, -1); },
 });
+
+function addIn(val, additive) {
+  let newVal = val + additive;
+  if (newVal < 0)
+    newVal = 0; // never let it go negative ... even if we have something out of sync
+  return [newVal, ()=>`setting inProgress to: ${newVal}`];
+}
 
 export default function inProgress(inProgress=0, action) {
   return subReducer.resolve(inProgress, action);
