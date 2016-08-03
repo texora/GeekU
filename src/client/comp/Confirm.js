bc99e9d6d7ebec00e65a04e9efe66ff230906bee
Kevin Bridges <kevin@wiiBridges.com>
2016-08-03 07:49:03 -0500

    provide a Confirm alias to the Alert component

---
+++ src/client/comp/Confirm.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import Confirm from './Alert';
+
+/**
+ * The Confirm utility provides a programmatic means by which user
+ * confirmations may be obtained through a modal dialog.
+ *
+ * NOTE: The Confirm component is merely an alias to the Alert
+ *       component.
+ *       Please refer to Alert for full documentation.
+ */
+
+export default Confirm;
