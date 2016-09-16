6e0bad899f3f561e228f6aa85bf00fc95fd67fa6
Kevin Bridges <kevin@wiiBridges.com>
2016-09-16 10:29:28 -0500

    javadoc updates

--- src/client/actions/actions.js
+++ src/client/actions/actions.js
@@ -22,9 +22,9 @@ import generate_AT_AC  from './generate_AT_AC';
  *
  * IMPORTANT:
  *
- *   In order to gain an understanding of any reactive application, it
- *   is imperative to fully understand it's emitted actions.  To that
- *   end, the following hash-tags are documented with each action:
+ *   In order to gain a better understanding of any reactive application, it
+ *   is important to fully understand it's emitted actions.  To that end,
+ *   the following hash-tags are documented with each action:
  *
  *     #byUser:    dispatched by user action      (i.e. initiated directly from UI)
  *     #byLogic:   dispatched by app logic        (i.e. sourced from other actions)
@@ -32,7 +32,7 @@ import generate_AT_AC  from './generate_AT_AC';
  *     #noReducer: of NO real interest to reducer (i.e. used to stimulate logic)
  *
  *   As an example, the 'selCrit.edit.save' action is of no interest to
- *   reducers (#noReducer) because app logic monitoring this action
+ *   reducers (#noReducer) because application logic monitoring this action
  *   will emit a more general action 'selCrit.changed' which provides
  *   a more central opportunity to maintain our state (#byLogic,
  *   #reducer).
@@ -94,24 +94,29 @@ import generate_AT_AC  from './generate_AT_AC';
 const genesis = {
 
   // ***
-  // *** display a user message via Material UI Snackbar
+  // *** Various User Notifications ...
   // ***
-  //       AC.userMsg.display(msg [,userAction])
-  //
-  //          NOTE: An alternate technique to activate a user message is through
-  //                the static UserMsg.display(msg [, userAction]) method.  This
-  //                may be preferred when:
-  //                  a) no other actions need to be 'batched' with the user
-  //                     message, and/or
-  //                  b) when you have NO access to the dispatcher.
-  //
-  //          @param {string} msg the message to display.
-  //          @param {Obj} userAction an optional structure defining a user click action:
-  //                         userAction: {  // optional action that can be activated by the user
-  //                           txt:      '',
-  //                           callback: function(event)
-  //                         }
-  'userMsg.display': { traits: ['msg', 'userAction'], 
+
+  /**
+   * AC.userMsg.display(msg [,userAction]) ...
+   *
+   * Display a user message via Material UI Snackbar.
+   *
+   * NOTE: An alternate technique to activate a user message is through
+   *       the static UserMsg.display(msg [, userAction]) method.  This
+   *       may be preferred when you have no access to the dispatcher.
+   *
+   * @param {string} msg the message to display.
+   *
+   * @param {Obj} userAction an optional structure defining a user click action:
+   *                {
+   *                  txt:      'your-button-label-here',
+   *                  callback: function(event) {
+   *                    code-executed-on-button-click
+   *                  }
+   *                }
+   */
+  'userMsg.display': { traits: ['msg', 'userAction'],  // ... #byUser, #byLogic, #reducer
                        ratify(msg, userAction) {
                          const errPrefix = () => {
                            const userActionStr = userAction ? `, ${JSON.stringify(userAction)}` : '';
@@ -124,22 +129,31 @@ const genesis = {
                          }
                          return [msg, userAction];
                        }},
-  'userMsg.close':   { traits: [] },
+  'userMsg.close':   { traits: [] },  // ... #byLogic, #reducer
 
 
   // ***
   // *** Items View related
   // ***
 
-  // retrieve and/or activate the Items View for the specified itemType (use when you wish to do BOTH)
-  //   * itemType:    the itemType ... 'student'/'course'
-  //   * retrieve:    the retrieval directive, one of:
-  //     - null:        no retrieval at all (DEFAULT)
-  //     - SelCrit:     conditionally retrieve items when supplied selCrit is different (or out-of-date) from ItemsView selCrit
-  //     - 'refresh':   unconditionally refresh ItemsView with latest items (using view's current selCrit)
-  //   * activate:    the activate directive, one of:
-  //     - 'activate':    activate/visualize this itemType ItemsView (DEFAULT for all but 'refresh' retrieval)
-  //     - 'no-activate': DO NOT activate                            (DEFAULT for 'refresh' retrieval)
+  /**
+   * AC.itemsView(itemType [, retrieve, activate]) ...
+   *
+   * Retrieve and/or activate the itemsView for the specified
+   * itemType.  Use this action when you wish to do BOTH
+   * retrieve/activate.
+   *
+   * @param {string} itemType the itemType ('student'/'course').
+   *
+   * @param {any} retrieve the retrieval directive, one of:
+   *   - null:        no retrieval at all (DEFAULT)
+   *   - SelCrit:     conditionally retrieve items when supplied selCrit is different (or out-of-date) from ItemsView selCrit
+   *   - 'refresh':   unconditionally refresh ItemsView with latest items (using view's current selCrit)
+   *
+   * @param {string} activate: an activate directive, one of:
+   *   - 'activate':    activate/visualize this itemType ItemsView (DEFAULT for all but 'refresh' retrieval)
+   *   - 'no-activate': DO NOT activate                            (DEFAULT for 'refresh' retrieval)
+   */
   'itemsView': { traits: ['itemType', 'retrieve', 'activate'], // ... #byUser, #noReducer
                  ratify(itemType,
                         retrieve=null,
@@ -157,10 +171,17 @@ const genesis = {
                  }},
 
 
-  // retrieve the Items View for the specified itemType
-  //   * selCrit:    the selCrit driving the retrieval, one of:
-  //     - SelCrit:     conditionally retrieve items when supplied selCrit is different (or out-of-date) from ItemsView selCrit
-  //     - 'refresh':   unconditionally refresh ItemsView with latest items (using view's current selCrit)
+  /**
+   * AC.itemsView.retrieve(itemType, selCrit) ...
+   *
+   * Retrieve the Items View for the specified itemType.
+   *
+   * @param {string} itemType the itemType ('student'/'course').
+   *
+   * @param {any} selCrit the selCrit driving the retrieval, one of:
+   *   - SelCrit:     conditionally retrieve items when supplied selCrit is different (or out-of-date) from ItemsView selCrit
+   *   - 'refresh':   unconditionally refresh ItemsView with latest items (using view's current selCrit)
+   */
   'itemsView.retrieve': { traits: ['itemType', 'selCrit'],  // ... #byUser, #byLogic, #reducer(spinner only)
                           ratify(itemType, selCrit) {
                             assert(itemTypes[itemType],
@@ -174,17 +195,29 @@ const genesis = {
   'itemsView.retrieve.fail':     { traits: ['itemType', 'selCrit', 'err'] },   // ... #byLogic, #reducer(spinner only)
 
 
-  // activate the Items View for the specified itemType
+  /**
+   * AC.itemsView.activate(itemType) ...
+   *
+   * Activate the Items View for the specified itemType.
+   *
+   * @param {string} itemType the itemType ('student'/'course').
+   */
   'itemsView.activate': { traits: ['itemType'] }, // ... #byUser, #byLogic, #reducer
 
 
   // ***
-  // *** select item
+  // *** selected item ...
   // ***
-  //       AC.selectItem(itemType, item)
-  //        * itemType:    the itemType ... 'student'/'course'
-  //        * item:        the item to select (null for de-select)
-  'selectItem': { traits: ['itemType', 'item'] },
+
+  /**
+   * AC.selectItem(itemType, item) ...
+   *
+   * Select an item within an itemsView.
+   *
+   * @param {string} itemType the itemType ('student'/'course').
+   * @param {any}    item the item to select (null for de-select)
+   */
+  'selectItem': { traits: ['itemType', 'item'] }, // ... #byUser, #byLogic, #reducer
 
 
   // ***
@@ -192,8 +225,9 @@ const genesis = {
   // ***
 
   /**
-   * AC.detailItem(itemType, itemNum, editMode): activate a dialog
-   * detailing (and/or editing) the supplied item.
+   * AC.detailItem(itemType, itemNum, editMode) ...
+   *
+   * Activate a dialog detailing (and/or editing) the supplied item.
    *
    * An up-to-date item image is retrieved prior to it's display.
    *
@@ -210,9 +244,14 @@ const genesis = {
 
 
   // ***
-  // *** retrieve filters (a list of selCrit objects)
+  // *** filters (i.e. selCrit objects) ...
   // ***
 
+  /**
+   * AC.filters.retrieve() ...
+   *
+   * Retrieve filters ... a list of selCrit objects.
+   */
   'filters.retrieve':          { traits: [] },          // ... #byLogic, #reducer(spinner only)
   'filters.retrieve.complete': { traits: ['filters'] }, // ... #byLogic, #reducer
   'filters.retrieve.fail':     { traits: ['err'] },     // ... #byLogic, #reducer(spinner only)
@@ -222,7 +261,20 @@ const genesis = {
   // *** edit selCrit (in interactive dialog)
   // ***
 
-  // initiate an edit session
+  /**
+   * AC.selCrit.edit(selCrit' [, isNew, syncDirective]) ...
+   *
+   * Start an edit dialog session of the supplied selCrit.
+   *
+   * @param {SelCrit} selCrit the selCrit to edit.
+   *
+   * @param {boolean} isNew indicator as to whether the supplied selCrit
+   * is new (true), or previously existed (false) ... DEFAULT: false
+   *
+   * @param {SelCrit.SyncDirective} syncDirective a directive that indicates
+   * how selCrit changes should be synced in selCrit-based views
+   * ... DEFAULT: SelCrit.SyncDirective.default
+   */
   'selCrit.edit': { traits: ['selCrit', 'isNew', 'syncDirective'], // ... #byUser, #reducer
                     ratify(selCrit,
                            isNew=false,
@@ -261,16 +313,20 @@ const genesis = {
   // ***
 
   /**
-   * AC.selCrit.changed(selCrit, syncDirective): is a central notification that the supplied
-   * selCrit has changed (and is in a completed/valid state).
+   * AC.selCrit.changed(selCrit [, syncDirective]) ...
    *
-   * This is emitted by various logic points under any circumstance of a completed/valid change
-   * (ex: edit dialog completion, save, etc.), and is of interest to reducers to to maintain 
-   * overall state.
+   * A central notification that the supplied selCrit has changed (and
+   * is in a completed/valid state).
+   *
+   * This is emitted by various logic points under any circumstance of
+   * a completed/valid change (ex: edit dialog completion, save,
+   * etc.), and is of interest to reducers to to maintain overall
+   * state.
    * 
    * @param {SelCrit} selCrit the selCrit that has changed.
    * @param {SelCrit.SyncDirective} syncDirective a directive that indicates
    * how selCrit changes should be synced in selCrit-based views.
+   * ... DEFAULT: SelCrit.SyncDirective.default
    */
   'selCrit.changed': { traits: ['selCrit', 'syncDirective'], // ... #byLogic, #reducer
                        ratify(selCrit,
@@ -287,6 +343,18 @@ const genesis = {
   // *** save specified selCrit
   // ***
 
+  /**
+   * AC.selCrit.save(selCrit [, syncDirective]) ...
+   *
+   * Save the supplied selCrit.
+   * 
+   * Any view that is based on this selCrit is automatically updated.
+   * 
+   * @param {SelCrit} selCrit the selCrit to save.
+   * @param {SelCrit.SyncDirective} syncDirective a directive that indicates
+   * how selCrit changes should be synced in selCrit-based views.
+   * ... DEFAULT: SyncDirective.default
+   */
   'selCrit.save': { traits: ['selCrit', 'syncDirective'], // ... #byUser, #byLogic, #reducer(spinner only)
                     ratify(selCrit,
                            syncDirective=SelCrit.SyncDirective.default) {
@@ -305,8 +373,9 @@ const genesis = {
   // ***
 
   /**
-   * AC.selCrit.delete(selCrit): delete the supplied selCrit, after obtaining
-   * a user confirmation.
+   * AC.selCrit.delete(selCrit) ...
+   *
+   * Delete the supplied selCrit, after obtaining a user confirmation.
    * 
    * Any view that is based on this selCrit is automatically updated (see impactView).
    * 
